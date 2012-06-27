971f41c9d350358d20a1ac686d89a32c6eb4af08
Miguel Gaiowski <miggaiowski@gmail.com>
2012-06-27 18:33:57 -0300

    Executa testes

--- clefia.c
+++ clefia.c
@@ -61,47 +61,48 @@ char s1[16][16] = {
   0xf7, 0xe4, 0x79, 0x96, 0xa2, 0xfc, 0x6d, 0xb2, 0x6b, 0x03, 0xe1, 0x2e, 0x7d, 0x14, 0x95, 0x1d};
  
 unsigned int con_128[60] = {  
-   0xf56b7aeb, 0x994a8a42, 0x96a4bd75, 0xfa854521,
-   0x735b768a, 0x1f7abac4, 0xd5bc3b45, 0xb99d5d62,
-   0x52d73592, 0x3ef636e5, 0xc57a1ac9, 0xa95b9b72,
-   0x5ab42554, 0x369555ed, 0x1553ba9a, 0x7972b2a2,
-   0xe6b85d4d, 0x8a995951, 0x4b550696, 0x2774b4fc,
-   0xc9bb034b, 0xa59a5a7e, 0x88cc81a5, 0xe4ed2d3f,
-   0x7c6f68e2, 0x104e8ecb, 0xd2263471, 0xbe07c765,
-   0x511a3208, 0x3d3bfbe6, 0x1084b134, 0x7ca565a7,
-   0x304bf0aa, 0x5c6aaa87, 0xf4347855, 0x9815d543,
-   0x4213141a, 0x2e32f2f5, 0xcd180a0d, 0xa139f97a,
-   0x5e852d36, 0x32a464e9, 0xc353169b, 0xaf72b274,
-   0x8db88b4d, 0xe199593a, 0x7ed56d96, 0x12f434c9,
-   0xd37b36cb, 0xbf5a9a64, 0x85ac9b65, 0xe98d4d32,
-   0x7adf6582, 0x16fe3ecd, 0xd17e32c1, 0xbd5f9f66,
-   0x50b63150, 0x3c9757e7, 0x1052b098, 0x7c73b3a7
+  0xf56b7aeb, 0x994a8a42, 0x96a4bd75, 0xfa854521,
+  0x735b768a, 0x1f7abac4, 0xd5bc3b45, 0xb99d5d62,
+  0x52d73592, 0x3ef636e5, 0xc57a1ac9, 0xa95b9b72,
+  0x5ab42554, 0x369555ed, 0x1553ba9a, 0x7972b2a2,
+  0xe6b85d4d, 0x8a995951, 0x4b550696, 0x2774b4fc,
+  0xc9bb034b, 0xa59a5a7e, 0x88cc81a5, 0xe4ed2d3f,
+  0x7c6f68e2, 0x104e8ecb, 0xd2263471, 0xbe07c765,
+  0x511a3208, 0x3d3bfbe6, 0x1084b134, 0x7ca565a7,
+  0x304bf0aa, 0x5c6aaa87, 0xf4347855, 0x9815d543,
+  0x4213141a, 0x2e32f2f5, 0xcd180a0d, 0xa139f97a,
+  0x5e852d36, 0x32a464e9, 0xc353169b, 0xaf72b274,
+  0x8db88b4d, 0xe199593a, 0x7ed56d96, 0x12f434c9,
+  0xd37b36cb, 0xbf5a9a64, 0x85ac9b65, 0xe98d4d32,
+  0x7adf6582, 0x16fe3ecd, 0xd17e32c1, 0xbd5f9f66,
+  0x50b63150, 0x3c9757e7, 0x1052b098, 0x7c73b3a7
 };
    
 unsigned int con_192[84] = {
-	0xc6d61d91, 0xaaf73771, 0x5b6226f8, 0x374383ec,
-	0x15b8bb4c, 0x799959a2, 0x32d5f596, 0x5ef43485,
-   	0xf57b7acb, 0x995a9a42, 0x96acbd65, 0xfa8d4d21,
-	0x735f7682, 0x1f7ebec4, 0xd5be3b41, 0xb99f5f62,
-	0x52d63590, 0x3ef737e5, 0x1162b2f8, 0x7d4383a6,
-	0x30b8f14c, 0x5c995987, 0x2055d096, 0x4c74b497,
-	0xfc3b684b, 0x901ada4b, 0x920cb425, 0xfe2ded25,
-	0x710f7222, 0x1d2eeec6, 0xd4963911, 0xb8b77763,
-	0x524234b8, 0x3e63a3e5, 0x1128b26c, 0x7d09c9a6,
-	0x309df106, 0x5cbc7c87, 0xf45f7883, 0x987ebe43,
-	0x963ebc41, 0xfa1fdf21, 0x73167610, 0x1f37f7c4,
-	0x01829338, 0x6da363b6, 0x38c8e1ac, 0x54e9298f,
-	0x246dd8e6, 0x484c8c93, 0xfe276c73, 0x9206c649,
-	0x9302b639, 0xff23e324, 0x7188732c, 0x1da969c6,
-	0x00cd91a6, 0x6cec2cb7, 0xec7748d3, 0x8056965b,
-	0x9a2aa469, 0xf60bcb2d, 0x751c7a04, 0x193dfdc2,
-	0x02879532, 0x6ea666b5, 0xed524a99, 0x8173b35a,
-	0x4ea00d7c, 0x228141f9, 0x1f59ae8e, 0x7378b8a8,
-	0xe3bd5747, 0x8f9c5c54, 0x9dcfaba3, 0xf1ee2e2a,
-	0xa2f6d5d1, 0xced71715, 0x697242d8, 0x055393de,
-	0x0cb0895c, 0x609151bb, 0x3e51ec9e, 0x5270b089
+  0xc6d61d91, 0xaaf73771, 0x5b6226f8, 0x374383ec,
+  0x15b8bb4c, 0x799959a2, 0x32d5f596, 0x5ef43485,
+  0xf57b7acb, 0x995a9a42, 0x96acbd65, 0xfa8d4d21,
+  0x735f7682, 0x1f7ebec4, 0xd5be3b41, 0xb99f5f62,
+  0x52d63590, 0x3ef737e5, 0x1162b2f8, 0x7d4383a6,
+  0x30b8f14c, 0x5c995987, 0x2055d096, 0x4c74b497,
+  0xfc3b684b, 0x901ada4b, 0x920cb425, 0xfe2ded25,
+  0x710f7222, 0x1d2eeec6, 0xd4963911, 0xb8b77763,
+  0x524234b8, 0x3e63a3e5, 0x1128b26c, 0x7d09c9a6,
+  0x309df106, 0x5cbc7c87, 0xf45f7883, 0x987ebe43,
+  0x963ebc41, 0xfa1fdf21, 0x73167610, 0x1f37f7c4,
+  0x01829338, 0x6da363b6, 0x38c8e1ac, 0x54e9298f,
+  0x246dd8e6, 0x484c8c93, 0xfe276c73, 0x9206c649,
+  0x9302b639, 0xff23e324, 0x7188732c, 0x1da969c6,
+  0x00cd91a6, 0x6cec2cb7, 0xec7748d3, 0x8056965b,
+  0x9a2aa469, 0xf60bcb2d, 0x751c7a04, 0x193dfdc2,
+  0x02879532, 0x6ea666b5, 0xed524a99, 0x8173b35a,
+  0x4ea00d7c, 0x228141f9, 0x1f59ae8e, 0x7378b8a8,
+  0xe3bd5747, 0x8f9c5c54, 0x9dcfaba3, 0xf1ee2e2a,
+  0xa2f6d5d1, 0xced71715, 0x697242d8, 0x055393de,
+  0x0cb0895c, 0x609151bb, 0x3e51ec9e, 0x5270b089
 };
 
+#define WORD_FROM_BYTES(_a,_b,_c,_d) (unsigned int)(((_a << 24) | 0x00ffffff) & ((_b << 16) | 0xff00ffff ) & ((_c << 8) | 0xffff00ff) & (_d | 0xffffff00));
 
 unsigned int word_from_bytes(char a, char b, char c, char d) {
   unsigned int res = 0;
@@ -110,16 +111,16 @@ unsigned int word_from_bytes(char a, char b, char c, char d) {
 }
 
 char byte_from_word(unsigned int x, int off) {
-	if (off == 3) {
-		return x & W24_32;	
-	}
-	if (off == 2) {
-		return (x & W16_24)	>> 8;
-	}
-	if (off == 1) {
-		return (x & W8_16) >> 16;
-	}
-	return (x & W0_8) >> 24;
+  if (off == 3) {
+    return x & W24_32;	
+  }
+  if (off == 2) {
+    return (x & W16_24)	>> 8;
+  }
+  if (off == 1) {
+    return (x & W8_16) >> 16;
+  }
+  return (x & W0_8) >> 24;
 }
 
 unsigned char mul2(unsigned char x) {
@@ -152,7 +153,7 @@ unsigned int f0(unsigned int rk, unsigned int x) {
   char y1 = mul2(t0) ^      t1  ^ mul6(t2) ^ mul4(t3);
   char y2 = mul4(t0) ^ mul6(t1) ^      t2  ^ mul2(t3);
   char y3 = mul6(t0) ^ mul4(t1) ^ mul2(t2) ^       t3;
-  unsigned int y = word_from_bytes(y0, y1, y2, y3);
+  unsigned int y = WORD_FROM_BYTES(y0, y1, y2, y3);
   return y;
 }
 
@@ -173,7 +174,7 @@ unsigned int f1(unsigned int rk, unsigned int x) {
   char y1 = mul8(t0) ^      t1  ^ mulA(t2) ^ mul2(t3);
   char y2 = mul2(t0) ^ mulA(t1) ^      t2  ^ mul8(t3);
   char y3 = mulA(t0) ^ mul2(t1) ^ mul8(t2) ^       t3;
-  return word_from_bytes(y0, y1, y2, y3);
+  return WORD_FROM_BYTES(y0, y1, y2, y3);
 }
 
 void gfn4(int r, unsigned int* rk, unsigned int *x, unsigned int *y) {
@@ -286,102 +287,102 @@ void sigma(unsigned int *x, unsigned int* y) {
 }
 
 void key_scheduling_192(unsigned int *k, unsigned int *wk, unsigned int* rk) {
-	unsigned int kl[4], kr[4], ll[4], lr[4], t[4];
-	unsigned int y[8], x[8];
-	int i;
+  unsigned int kl[4], kr[4], ll[4], lr[4], t[4];
+  unsigned int y[8], x[8];
+  int i;
 		
-	/*step 2*/
-	kl[0] = k[0];
-	kl[1] = k[1];
-	kl[2] = k[2];
-	kl[3] = k[3];
+  /*step 2*/
+  kl[0] = k[0];
+  kl[1] = k[1];
+  kl[2] = k[2];
+  kl[3] = k[3];
 	
-	kr[0] = k[4];
-	kr[1] = k[5];
-	kr[2] = ~k[0];
-	kr[3] = ~k[1];
+  kr[0] = k[4];
+  kr[1] = k[5];
+  kr[2] = ~k[0];
+  kr[3] = ~k[1];
 	
-	/*step 3*/
-	x[0] = kl[0];
-	x[1] = kl[1];
-	x[2] = kl[2];
-	x[3] = kl[3];
-	x[4] = kr[0];
-	x[5] = kr[1];
-	x[6] = kr[2];
-	x[7] = kr[3];
+  /*step 3*/
+  x[0] = kl[0];
+  x[1] = kl[1];
+  x[2] = kl[2];
+  x[3] = kl[3];
+  x[4] = kr[0];
+  x[5] = kr[1];
+  x[6] = kr[2];
+  x[7] = kr[3];
 	
-	gfn8(10, con_192, x, y);
+  gfn8(10, con_192, x, y);
 	
-	ll[0] = y[0];
-	ll[1] = y[1];
-	ll[2] = y[2];
-	ll[3] = y[3];
+  ll[0] = y[0];
+  ll[1] = y[1];
+  ll[2] = y[2];
+  ll[3] = y[3];
 	
-	lr[0] = y[4];
-	lr[1] = y[5];
-	lr[2] = y[6];
-	lr[3] = y[7];
+  lr[0] = y[4];
+  lr[1] = y[5];
+  lr[2] = y[6];
+  lr[3] = y[7];
 	
   printf("LL: \n");
   for (i = 0; i < 4; i++) {
-  	printf("%08x ", ll[i]);
+    printf("%08x ", ll[i]);
   }
   printf("\n");
   
   printf("LR: \n");
   for (i = 0; i < 4; i++) {
-  	printf("%08x ", lr[i]);
+    printf("%08x ", lr[i]);
   }
   printf("\n");
 	
-	/* step 4 */
-	wk[0] = kl[0] ^ kr[0];
-	wk[1] = kl[1] ^ kr[1];
-	wk[2] = kl[2] ^ kr[2];
-	wk[3] = kl[3] ^ kr[3];
+  /* step 4 */
+  wk[0] = kl[0] ^ kr[0];
+  wk[1] = kl[1] ^ kr[1];
+  wk[2] = kl[2] ^ kr[2];
+  wk[3] = kl[3] ^ kr[3];
 	
-	/* step 5 */
-	for (i = 0; i <= 10; i++) {
-		if (i % 4 == 0 || i % 4 == 1) {
-			t[0] = ll[0] ^ con_192[40 + 4*i];
-			t[1] = ll[1] ^ con_192[40 + 4*i + 1];
-			t[2] = ll[2] ^ con_192[40 + 4*i + 2];
-			t[3] = ll[3] ^ con_192[40 + 4*i + 3];
-			sigma(ll, y);
-			ll[0] = y[0];
-			ll[1] = y[1];
-			ll[2] = y[2];
-			ll[3] = y[3];
-			if (i % 2 == 1) {
-				t[0] = t[0] ^ kr[0];
-				t[1] = t[1] ^ kr[1];
-				t[2] = t[2] ^ kr[2];
-				t[3] = t[3] ^ kr[3];	
-			}	
-		}
-		else {
-			t[0] = lr[0] ^ con_192[40 + 4*i];
-			t[1] = lr[1] ^ con_192[40 + 4*i + 1];
-			t[2] = lr[2] ^ con_192[40 + 4*i + 2];
-			t[3] = lr[3] ^ con_192[40 + 4*i + 3];
-			sigma(lr, y);
-			lr[0] = y[0];
-			lr[1] = y[1];
-			lr[2] = y[2];
-			lr[3] = y[3];
-			if (i % 2 == 1) {
-				t[0] = t[0] ^ kl[0];
-				t[1] = t[1] ^ kl[1];
-				t[2] = t[2] ^ kl[2];
-				t[3] = t[3] ^ kl[3];
-			}
-		}
-		rk[4*i] = t[0];
-		rk[4*i+1] = t[1];
-		rk[4*i+2] = t[2];
-		rk[4*i+3] = t[3];	
-	}
+  /* step 5 */
+  for (i = 0; i <= 10; i++) {
+    if (i % 4 == 0 || i % 4 == 1) {
+      t[0] = ll[0] ^ con_192[40 + 4*i];
+      t[1] = ll[1] ^ con_192[40 + 4*i + 1];
+      t[2] = ll[2] ^ con_192[40 + 4*i + 2];
+      t[3] = ll[3] ^ con_192[40 + 4*i + 3];
+      sigma(ll, y);
+      ll[0] = y[0];
+      ll[1] = y[1];
+      ll[2] = y[2];
+      ll[3] = y[3];
+      if (i % 2 == 1) {
+        t[0] = t[0] ^ kr[0];
+        t[1] = t[1] ^ kr[1];
+        t[2] = t[2] ^ kr[2];
+        t[3] = t[3] ^ kr[3];	
+      }	
+    }
+    else {
+      t[0] = lr[0] ^ con_192[40 + 4*i];
+      t[1] = lr[1] ^ con_192[40 + 4*i + 1];
+      t[2] = lr[2] ^ con_192[40 + 4*i + 2];
+      t[3] = lr[3] ^ con_192[40 + 4*i + 3];
+      sigma(lr, y);
+      lr[0] = y[0];
+      lr[1] = y[1];
+      lr[2] = y[2];
+      lr[3] = y[3];
+      if (i % 2 == 1) {
+        t[0] = t[0] ^ kl[0];
+        t[1] = t[1] ^ kl[1];
+        t[2] = t[2] ^ kl[2];
+        t[3] = t[3] ^ kl[3];
+      }
+    }
+    rk[4*i] = t[0];
+    rk[4*i+1] = t[1];
+    rk[4*i+2] = t[2];
+    rk[4*i+3] = t[3];	
+  }
 }
 
 
@@ -548,87 +549,113 @@ void decryption_128(unsigned int *p, unsigned int *c, unsigned int *wk, unsigned
 
 void clefia_cbc_128_dec(char * plain, char * cipher, int length, unsigned int* iv, unsigned int *k) {
 	
-	unsigned int p[4];
-	unsigned int c[4];
-	unsigned int aux[4];
+  unsigned int p[4];
+  unsigned int c[4];
+  unsigned int aux[4];
 	
-	unsigned int wk[4];
-	unsigned int rk[36];
-	
-	
-	int j;
-	int i = 0;
-	
-	int tam = length;
-	
-	if (tam % 16 != 0) { 
-		for (j = tam % 16; j < 16; j++) {
-			cipher[tam++] = 0;	
-		}
-	}
-	
-	for (j = 0; j < 4; j++) {
-		aux[j] = iv[j];	
-	}
+  unsigned int wk[4];
+  unsigned int rk[36];
 	
 	
-	while (i < tam) {
-		for (j = 0; j < 4; j++) {
-			c[j] = word_from_bytes(cipher[i+4*j], cipher[i+4*j+1], cipher[i+4*j+2], cipher[i+4*j+3]);
-		}
-		decryption_128(p, c, wk, rk);
+  int j;
+  int i = 0;
+	
+  int tam = length;
+	
+  if (tam % 16 != 0) { 
+    for (j = tam % 16; j < 16; j++) {
+      cipher[tam++] = 0;	
+    }
+  }
+	
+  for (j = 0; j < 4; j++) {
+    aux[j] = iv[j];	
+  }
+	
+  key_scheduling_128(k, wk, rk);
+	
+  while (i < tam) {
+    for (j = 0; j < 4; j++) {
+      c[j] = WORD_FROM_BYTES(cipher[i+4*j], cipher[i+4*j+1], cipher[i+4*j+2], cipher[i+4*j+3]);
+    }
+    decryption_128(p, c, wk, rk);
 		
-		for (j = 0; j < 16; j++) {
-			plain[i+j] = byte_from_word(p[j/4], j%4) ^ byte_from_word(aux[j/4], j%4);
-		}
+    for (j = 0; j < 16; j++) {
+      plain[i+j] = byte_from_word(p[j/4], j%4) ^ byte_from_word(aux[j/4], j%4);
+    }
 		
-		for (j = 0; j < 4; j++) {
-			aux[j] = c[j];	
-		}
+    for (j = 0; j < 4; j++) {
+      aux[j] = c[j];	
+    }
 		
-		i = i + 16;
-	}	
+    i = i + 16;
+  }	
 }
 
 void clefia_cbc_128_enc(char* plain, char * cipher, int length, unsigned int* iv, unsigned int *k) {
 	
-	unsigned int p[4];
-	unsigned int c[4];
-	unsigned int wk[4];
-	unsigned int rk[36];
+  unsigned int p[4];
+  unsigned int c[4];
+  unsigned int wk[4];
+  unsigned int rk[36];
 	
-	int j;
-	int i = 0;
+  int j;
+  int i = 0;
 	
-	int tam = length;
+  int tam = length;
 	
-	if (tam % 16 != 0) {
-		for (j = tam % 16; j < 16; j++) {
-			plain[tam++] = 0;	
-		}	
-	}
+  if (tam % 16 != 0) {
+    for (j = tam % 16; j < 16; j++) {
+      plain[tam++] = 0;	
+    }	
+  }
 	
-	c[0] = iv[0];
-	c[1] = iv[1];
-	c[2] = iv[2];
-	c[3] = iv[3];
+  c[0] = iv[0];
+  c[1] = iv[1];
+  c[2] = iv[2];
+  c[3] = iv[3];
 	
-	key_scheduling_128(k, wk, rk);
+  key_scheduling_128(k, wk, rk);
 	
-	while (i < tam) {
-		 
+  while (i < tam) {
+		 		
+    /* for (j = 0; j < 4; j++) { */
+    /*   p[j] = WORD_FROM_BYTES(plain[i+4*j], plain[i+4*j+1], plain[i+4*j+2], plain[i+4*j+3]) ^ c[j]; */
+    /* } */
+    unsigned int bla0 = WORD_FROM_BYTES(plain[i+4*0],plain[i+4*0+1],plain[i+4*0+2],plain[i+4*0+3]);
+    unsigned int bla1 = WORD_FROM_BYTES(plain[i+4*1],plain[i+4*1+1],plain[i+4*1+2],plain[i+4*1+3]);
+    unsigned int bla2 = WORD_FROM_BYTES(plain[i+4*2],plain[i+4*2+1],plain[i+4*2+2],plain[i+4*2+3]);
+    unsigned int bla3 = WORD_FROM_BYTES(plain[i+4*3],plain[i+4*3+1],plain[i+4*3+2],plain[i+4*3+3]);
+
+    p[0] = bla0 ^ c[0];
+    p[1] = bla1 ^ c[1];
+    p[2] = bla2 ^ c[2];
+    p[3] = bla3 ^ c[3];
+    
 		
-		for (j = 0; j < 4; j++) {
-			p[j] = word_from_bytes(plain[i+4*j], plain[i+4*j+1], plain[i+4*j+2], plain[i+4*j+3]) ^ c[j];
-		}
+    encryption_128(p, c, wk, rk);
 		
-		encryption_128(p, c, wk, rk);
-		
-		for (j = 0; j < 16; j++) {
-			cipher[i+j] = byte_from_word(c[j/4], j % 4); 
-		}
-		i = i + 16;		
-	}
+    /* for (j = 0; j < 16; j++) { */
+    /*   cipher[i+j] = byte_from_word(c[j/4], j % 4);  */
+    /* } */
+    cipher[i+0] = byte_from_word(c[0/4], 0 % 4);
+    cipher[i+1] = byte_from_word(c[1/4], 1 % 4);
+    cipher[i+2] = byte_from_word(c[2/4], 2 % 4);
+    cipher[i+3] = byte_from_word(c[3/4], 3 % 4);
+    cipher[i+4] = byte_from_word(c[4/4], 4 % 4);
+    cipher[i+5] = byte_from_word(c[5/4], 5 % 4);
+    cipher[i+6] = byte_from_word(c[6/4], 6 % 4);
+    cipher[i+7] = byte_from_word(c[7/4], 7 % 4);
+    cipher[i+8] = byte_from_word(c[8/4], 8 % 4);
+    cipher[i+9] = byte_from_word(c[9/4], 9 % 4);
+    cipher[i+10] = byte_from_word(c[10/4], 10 % 4);
+    cipher[i+11] = byte_from_word(c[11/4], 11 % 4);
+    cipher[i+12] = byte_from_word(c[12/4], 12 % 4);
+    cipher[i+13] = byte_from_word(c[13/4], 13 % 4);
+    cipher[i+14] = byte_from_word(c[14/4], 14 % 4);
+    cipher[i+15] = byte_from_word(c[15/4], 15 % 4);
+    i = i + 16;		
+  }
 }
 
 /* Retorna 1 se vetores são iguais, 0 c.c. */
@@ -641,59 +668,116 @@ int equal(unsigned int* a, unsigned int* b, unsigned int size) {
   return 1;
 }
 
-int main() {
+int main(int argc, char **argv) {
   /* Teste 128-bit */
-  unsigned int key_128[4] = {0xffeeddcc, 
-                         0xbbaa9988,
-                         0x77665544,
-                         0x33221100};
+  /* unsigned int key_128[4] = {0xffeeddcc,  */
+  /*                        0xbbaa9988, */
+  /*                        0x77665544, */
+  /*                        0x33221100}; */
                          
-   unsigned int key_192[6] = {0xffeeddcc, 0xbbaa9988, 0x77665544, 0x33221100,
-              					0xf0e0d0c0, 0xb0a09080};
+  /*  unsigned int key_192[6] = {0xffeeddcc, 0xbbaa9988, 0x77665544, 0x33221100, */
+  /*             					0xf0e0d0c0, 0xb0a09080}; */
               					
-  unsigned int plaintext_128[4] = {0x00010203,
-                               0x04050607,
-                               0x08090a0b,
-                               0x0c0d0e0f};
-  unsigned int ciphertext_128[4] = {0xde2bf2fd,
-                                0x9b74aacd,
-                                0xf1298555,
-                                0x459494fd};
+  /* unsigned int plaintext_128[4] = {0x00010203, */
+  /*                              0x04050607, */
+  /*                              0x08090a0b, */
+  /*                              0x0c0d0e0f}; */
+  /* unsigned int ciphertext_128[4] = {0xde2bf2fd, */
+  /*                               0x9b74aacd, */
+  /*                               0xf1298555, */
+  /*                               0x459494fd}; */
                                 
-  unsigned int plaintext_192[4] = {0x00010203,
-                               0x04050607,
-                               0x08090a0b,
-                               0x0c0d0e0f};
-  unsigned int ciphertext_192[4] = {0xe2482f64, 0x9f028dc4, 0x80dda184, 0xfde181ad};            
+  /* unsigned int plaintext_192[4] = {0x00010203, */
+  /*                              0x04050607, */
+  /*                              0x08090a0b, */
+  /*                              0x0c0d0e0f}; */
+  /* unsigned int ciphertext_192[4] = {0xe2482f64, 0x9f028dc4, 0x80dda184, 0xfde181ad};             */
                      
 
-  unsigned int encrypted[4];
-  unsigned int decrypted[4];
-  
- /* encryption_128(plaintext_128, encrypted, key_128);
-  decryption_128(decrypted, encrypted, key_128);
+  /* unsigned int encrypted[4]; */
+  /* unsigned int decrypted[4]; */
 
-  if (equal(ciphertext_128, encrypted, 4)) {
-    printf("Encryption 128 works.\n");
+  /* encryption_128(plaintext_128, encrypted, key_128);
+     decryption_128(decrypted, encrypted, key_128);
+
+     if (equal(ciphertext_128, encrypted, 4)) {
+     printf("Encryption 128 works.\n");
+     }
+     if (equal(plaintext_128, decrypted, 4)) {
+     printf("Decryption 128 works.\n");
+     }*/
+
+  if (argc < 4) {
+    printf("Usage: %s <entrada> <cifrado> <decifrado>", argv[0]); 
+    exit(1);
   }
-  if (equal(plaintext_128, decrypted, 4)) {
-    printf("Decryption 128 works.\n");
-  }*/
+
+  unsigned int key_128[4] = {0x01234567,
+                             0x89abcdef,
+                             0x01234567,
+                             0x89abcdef};
+
+  unsigned int iv_128[4] = {0x01234567,
+                            0x89abcdef,
+                            0x01234567,
+                            0x89abcdef};
+
+  char *data = (char*)malloc(sizeof(char) * 300 * 1024 * 1024);
+
+  FILE *f;
+  int nread;
+
+  /* printf("Lendo entrada\n"); */
+  /* f = fopen(argv[1], "r"); */
+  /* nread = fread(data, sizeof(char), 300*1024*1024, f); */
+  /* fclose(f); */
+  /* printf("Bytes read: %d\n", nread); */
   
- 	/*
- 	encryption_192(plaintext_192, encrypted, key_192);
+  char *cipher = (char*)malloc(sizeof(char) * 300 * 1024 * 1024);
+
+  /* printf("Cifrando...\n"); */
+  /* clefia_cbc_128_enc(data, cipher, nread, iv_128, key_128); */
+  /* printf("Cifragem completa\n"); */
+
+  /* printf("Salvando arquivo cifrado %s...\n", argv[2]);   */
+  /* f = fopen(argv[2], "w"); */
+  /* fwrite(cipher, sizeof(char), nread, f); */
+  /* fclose(f); */
+  /* printf("Arquivo salvo\n"); */
+
+  printf("Lendo arquivo cifrado\n");
+  f = fopen(argv[2], "r");
+  nread = fread(cipher, sizeof(char), 300*1024*1024, f);
+  fclose(f);
+
+  printf("Decifrando...\n");
+  clefia_cbc_128_dec(data, cipher, nread, iv_128, key_128);
+  printf("Decifragem completa\n");
+
+  printf("Salvando arquivo decifrado %s...\n", argv[2]);
+  f = fopen(argv[3], "w");
+  fwrite(data, sizeof(char), nread, f);
+  fclose(f);
+  printf("Arquivo salvo\n");
+  
+  free(cipher);
+  free(data);
+
+  
+  /*
+    encryption_192(plaintext_192, encrypted, key_192);
   
 
   
- 	decryption_192(decrypted, encrypted, key_192);
+    decryption_192(decrypted, encrypted, key_192);
 
-	if (equal(ciphertext_192, encrypted, 4)) {
-		printf("Encryption 192 works.\n");
-	}
-	if (equal(plaintext_192, decrypted, 4)) {
-	    printf("Decryption 192 works.\n");
-	}
-	*/
+    if (equal(ciphertext_192, encrypted, 4)) {
+    printf("Encryption 192 works.\n");
+    }
+    if (equal(plaintext_192, decrypted, 4)) {
+    printf("Decryption 192 works.\n");
+    }
+  */
 	
 	
 	
